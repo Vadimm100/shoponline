4652c5401b6a88b43083c61fe1125ce591b18950
Vadimm100 <vadimnamber1@mail.ru>
2023-03-15 23:17:58 +0300

    start project

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActiv = db.Column(db.Boolean, default=True)
+    # img = db.Column(db.Varbinary, 'C:\1.jpg')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+
+@app.route('/com')
+def communications():
+    return render_template('communications.html')
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
