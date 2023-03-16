8d5373e19394e961efdfd493dd3ca6eef3b57978
Vadimm100 <vadimnamber1@mail.ru>
2023-03-16 23:27:10 +0300

    start project

--- app.py
+++ app.py
@@ -6,26 +6,44 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     price = db.Column(db.Integer, nullable=False)
     isActiv = db.Column(db.Boolean, default=True)
-    # img = db.Column(db.Varbinary, 'C:\1.jpg')
+
+    def __repr__(self):
+        return self.title
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/create')
+
+@app.route('/create', methods=['POST', 'Get'])
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Произошла ошибка'
+    else:
+        return render_template('create.html')
 
 
 @app.route('/com')
